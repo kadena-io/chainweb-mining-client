f031bcacd12584060f34a459df3fd7149abe7bd2
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-07-04 12:06:17 +0200

    Add a flake.nix that builds with haskell.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Chainweb Mining Client";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          # This overlay adds our project to pkgs
+          chainweb-mining-client =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc810";
+              # This is used by `nix develop .` to open a shell for use with
+              # `cabal`, `hlint` and `haskell-language-server`
+              shell.tools = {
+                cabal = {};
+                hlint = {};
+                haskell-language-server = {};
+              };
+              # Non-Haskell shell tools go here
+              shell.buildInputs = with pkgs; [
+                nixpkgs-fmt
+              ];
+              # This adds `js-unknown-ghcjs-cabal` to the shell.
+              # shell.crossPlatforms = p: [p.ghcjs];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.chainweb-mining-client.flake {};
+    in flake // {
+      # Built by `nix build .`
+      packages.default = flake.packages."chainweb-mining-client:exe:chainweb-mining-client";
+    });
+}
