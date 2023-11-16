c8e96160fd1fb26ec5cdeab0b53be036786a9aea
Enis Bayramoğlu <enisbayramoglu@gmail.com>
2023-11-16 13:05:41 +0100

    Turn default.nix into a function
    In order to maintain backwards compatibility with old consumers

--- default.nix
+++ default.nix
@@ -1,3 +1,6 @@
+{
+  ... # Maintain backwards compatibility with old consumers
+}:
 (import (
   fetchTarball {
     url = "https://github.com/edolstra/flake-compat/archive/12c64ca55c1014cdc1b16ed5a804aa8576601ff2.tar.gz";
